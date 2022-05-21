8e9e3e232a6f0fb672c8770d723309f7168419b3
PierreDeuj <al.mason@yandex.ru>
2022-05-21 13:37:57 +0300

    Две первые задачи

--- python_labs/task1.py
+++ python_labs/task1.py
@@ -9,3 +9,30 @@ def calc(a, b, x) -> float:
 
 if __name__ == "__main__":
     print(calc(2.5, 4.6, 1.15))
+
+# 18 вариант
+def task_example(a, b, x):
+    chisl = (a + b*x)**2,5
+    znamen = 1 + math.log(a + b*x)
+    y = chisl / znamen
+    return y
+
+def task_a(a, b, xn, xk, dx):
+    x = xn
+    y = []
+    while x <= xk:
+        y_tmp = calc(a, b, x)
+        y.append(y_tmp)
+        x += dx
+    return y
+
+def task_b(a, b, x):
+    y = []
+    for item in x:
+        y.append(calc(a, b, item))
+    return y
+
+if __name__ == "__main__":
+    print(task_a(2.5, 4.6, 1.1))
+    y = task_a()
+
