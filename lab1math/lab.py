3b572458e1125c1e50655412bd3ab01882ad8f58
Руслан Поляков <71173176+Ruslan-Polakov@users.noreply.github.com>
2022-05-21 13:32:12 +0300

    primer

---
+++ lab1math/lab.py
@@ -0,0 +1,9 @@
+import math
+
+def calc(a:float, b:float, x:float) -> float:
+    chisl =  (math.sin(a + b*x))**3.5
+    znamen = 1 + math.cos(abs(math.log(a + b*x, 2)))
+    y= chisl / znamen
+    return y
+    print (calc(2.5, 4.6, 1.15))
+    pass
