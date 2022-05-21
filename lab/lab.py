b909eabe19505e4d660682713b8ad271f8bb3bf3
MrOlivander <105809612+MrOlivander@users.noreply.github.com>
2022-05-21 13:43:21 +0300

    Labalatornaya
    qq

---
+++ lab/lab.py
@@ -0,0 +1,12 @@
+import math
+
+def calc(x:float)-> float:
+    return (math.asin(x)**4 + math.acos(x)**6)**(1/7)  
+#def task_a(хn,хk,dx):
+  #   pass
+#def task_b():
+if __name__ == "__main__":
+def task_a(хn,хk,dx):
+    pass
+    x = []
+    print(calc(0.14))
