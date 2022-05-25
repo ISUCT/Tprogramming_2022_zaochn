e3b67f137ee543ee8dae83f612ae81d47b6b65c5
Vitalik3744 <79621581640@yandex.ru>
2022-05-26 02:45:32 +0300

    Programm_test

---
+++ .vscode/Zadanie/test_Zadanie.py
@@ -0,0 +1,11 @@
+import unittest
+import Ninga
+
+class TestNinga(unittest.TestCase):
+
+    def test_five(self):
+        self.assertAlmostEqual("Я Одолею этих ниндзя")
+    
+     
+    if __name__ == '__main__':
+        unittest.main(5)
