3312d283ad28c1227b0334daf318d000ff954f21
Seneca33 <78662116+Seneca33@users.noreply.github.com>
2022-05-25 22:56:46 +0300

    3

--- lab1/summ_test.py
+++ lab1/summ_test.py
@@ -5,23 +5,23 @@ class TestSumm(unittest.TestCase):
 
     def test_positive(self):
         res = exam.summ(15, 20)
-        self.assertEqual(1, res)
+        self.assertEqual(res)
     
     def test_negative(self):
         res = exam.summ(-15, -20)
-        self.assertEqual(-1, res)
+        self.assertEqual(res)
     
     def test_first_negative(self):
         res = exam.summ(-15, 20)
-        self.assertEqual(1, res)
+        self.assertEqual(res)
 
     def test_second_negative(self):
         res = exam.summ(15, -20)
-        self.assertEqual(-1, res)
+        self.assertEqual(res)
 
     def test_zero(self):
         res = exam.summ(0, 0)
-        self.assertEqual(0, res)
+        self.assertEqual(res)
 
 if __name__ == '__main__':
     unittest.main()
