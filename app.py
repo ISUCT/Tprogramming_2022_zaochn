dedbc7aa908610b9ae1dc886ddbffd4690483c88
PierreDeuj <al.mason@yandex.ru>
2022-05-27 10:40:31 +0300

    Flask task added

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    now = datetime.now()
+    return f"<p>Hello, World! {now}</p>"
+    
