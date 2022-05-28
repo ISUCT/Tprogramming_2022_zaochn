f3cae1015b3bbbea3d23742fabc022701be8bc7e
PavelSlaanesh <PavelVictori96@yandex.ru>
2022-05-28 12:54:44 +0300

    попытки в картинку

--- app.py
+++ app.py
@@ -11,8 +11,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    now = datetime.now()
     user = {'username': 'Павлик'}
-    return render_template('index.html', title='home', user=user)
+    if now.hour > 5 and now.hour < 12:
+        time_of_day = "morning"
+    elif now.hour >= 12 and now.hour < 17:
+        time_of_day = "day"
+    elif now.hour >= 17 and now.hour < 23:
+        time_of_day = "evening"
+    items = ["один", "два", "три", "четыре", "пять"]
+
+    return render_template('index.html', title='home', user=user, time_of_day=time_of_day, items=items, )
 
 @app.route("/Hello")
 def hello_world():
