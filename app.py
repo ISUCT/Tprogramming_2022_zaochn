a52a75ea1bb2432161dd7dc0fcc7c80da0c355da
Kombatushka <105965507+Kombatushka@users.noreply.github.com>
2022-05-28 10:58:58 +0300

    1

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 def hello_world():
     now = datetime.now()
     return f"<p>Hello, World! {now}</p>"
+
+@app.route("/styled")
+def hello_styles():
+    now = datetime.now()
+    return f"""
+    <h1>Заголовок</h1>
+    <p>hello_world!</p>
+    <p>Текущее время {now} </p>
+    """
