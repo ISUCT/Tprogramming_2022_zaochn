e55f8d6be7bc66797eae45e87f2d3e55aa16c636
PavelSlaanesh <PavelVictori96@yandex.ru>
2022-05-28 11:19:19 +0300

    Омниссия спаси мой код

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
+from turtle import title
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
 from flask import Flask
 from datetime import datetime
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    user = {'username': 'Павлик'}
+    return render_template('index.html', title='home', user=user)
+
+@app.route("/Hello")
 def hello_world():
     now = datetime.now()
     return f"<p>О ПРЕКРАСНЫЙ СУП НАВАРИЛИ! {now}</p>"
+
+@app.route("/styled")
+def hello_styles():
+    now = datetime.now()
+    return f"""
+    <h1>Заголовушка</h1>
+    <p>О ПРЕКРАСНЫЙ СУП НАВАРИЛИ!</p>
+    <p>Времы судного дня {now}</p>
+    """
